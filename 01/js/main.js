686e83fafd57b0156c2a5ca73e9e2c5e0e796247
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-09-22 10:27:03 +0300

    'lesson01'

---
+++ 01/js/main.js
@@ -0,0 +1,54 @@
+// console.log('Hello world');
+
+// var a = 5;
+// let b = 10;
+// const c = 'hello';
+
+// let $ = 5;
+// let _ = 10;
+
+// let name;
+// let NAME;
+
+// let muCurrentJob = 'Frontend developer';
+
+// console.log('A: ', a);
+// console.log('B: ', b);
+// console.log('C: ', c);
+
+// Data Types
+
+// let str = 'Hello World';
+// let num = 10;
+// let bool = true;
+// let bigNumber = 14n;
+// let nullish = null;
+// let undef = undefined;
+// let symbol = Symbol('id');
+
+// let obj = {};
+
+// const myFunc = () => console.log('myFunc');
+
+// console.log('Result', typeof str);
+// console.log('Result', typeof num);
+// console.log('Result', typeof bool);
+// console.log('Result', typeof bigNumber);
+// console.log('Result', typeof nullish);
+// console.log('Result', typeof undef);
+// console.log('Result', typeof symbol);
+// console.log('Result', typeof symbol);
+// console.log('Result', typeof myFunc);
+
+// alert('Are you robot?');
+
+// let userAge = prompt('How old are you?');
+// console.log('userAge', userAge);
+// confirm('Are you Frontend developer?');
+
+// let strQ1 = 'Hello World';
+// let strQ2 = "Hello World";
+
+// let strExample = 45;
+// let strQ3 = `Your age is ${strExample}`;
+// console.log('strQ3', strQ3);
