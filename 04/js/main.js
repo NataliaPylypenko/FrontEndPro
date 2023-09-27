620e4ddae364fb5f35c615557e9a587a2cfa598b
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-09-27 11:42:59 +0300

    homework04

--- 04/js/main.js
+++ 04/js/main.js
@@ -1,5 +1,9 @@
 let time = prompt('Enter the number of hours to convert them to seconds');
 time = parseInt(time);
-let seconds = time * 60 * 60;
 
-alert(`There are ${seconds} seconds at ${time} hours`);
+if (time) {
+    let seconds = time * 60 * 60;
+    alert(`There are ${seconds} seconds at ${time} hours`);
+} else {
+    alert('You must enter number of hours!!!');
+}
