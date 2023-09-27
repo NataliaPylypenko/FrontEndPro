5a6c9246d91aa7e9e99d61ce5b42048b3b496cc3
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-09-27 12:25:21 +0300

    homework05

--- 05/js/main.js
+++ 05/js/main.js
@@ -1,6 +1,18 @@
 let firstNum = prompt('Enter first number');
 let secondNum = prompt('Enter second number');
 let thirdNum = prompt('Enter third number');
-let arithmeticMean = (Number(firstNum) + Number(secondNum) + Number(thirdNum)) / 3;
 
-alert(`${arithmeticMean} is the arithmetic mean of ${firstNum}, ${secondNum} and ${thirdNum}`);
+firstNum = Number(firstNum);
+secondNum = Number(secondNum);
+thirdNum = Number(thirdNum);
+
+let answer;
+
+if ((firstNum || firstNum === 0) && (secondNum || secondNum === 0) && (thirdNum || thirdNum === 0)) {
+    let arithmeticMean = (firstNum + secondNum + thirdNum) / 3;
+    answer = `${arithmeticMean} is the arithmetic mean of ${firstNum}, ${secondNum} and ${thirdNum}`;
+} else {
+    answer = 'You must enter numbers in all fields';
+}
+
+alert(answer);
