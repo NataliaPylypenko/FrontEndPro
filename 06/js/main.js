49dc9f7cc3858bd0eecc449baac2969e654fd215
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-10-04 11:46:57 +0300

    homework06

--- 06/js/main.js
+++ 06/js/main.js
@@ -1,51 +1,51 @@
 let message = '';
-let currentYear = new Date().getFullYear();
 
+let currentYear = new Date().getFullYear();
 let birthYear = prompt('What is your year of birth?');
-if(!isNaN(birthYear) && parseInt(birthYear) > 1923 && parseInt(birthYear) <= currentYear) {
+if(!isNaN(birthYear) && parseInt(birthYear) > currentYear - 120 && parseInt(birthYear) <= currentYear) {
     message += `You are ${currentYear - birthYear} years old. `
-} else if (birthYear === null) {
+} else if (birthYear === null || getTrimString(birthYear) === '') {
     alert('It is a pity that you did not want to enter your year of birth(');
 } else {
-    alert('You did not enter anything or entered an incorrect value!!!');
+    alert('You entered an incorrect value!!!');
 }
 
 let city = prompt('In which city do you live?');
-city = upFirsLetter(getTrimString(city));
-if (city === null) {
+city = getTrimString(city);
+if (city === null || city === '') {
     alert('It is a pity that you did not want to enter the city in which you live(');
-} else if (city === '') {
+} else if (!isNaN(city)) {
     alert('You did not enter anything!!!');
 } else {
-    getCountry(city);
+    getCountry(city.toLowerCase());
 }
 
 let sport = prompt('What is your favorite sport?');
 sport = getTrimString(sport);
-if (sport === null) {
+if (sport === null || sport === '') {
     alert('It is a pity that you did not want to enter your favorite sport(');
-} else if (sport === '') {
+} else if (!isNaN(sport)) {
     alert('You did not enter anything!!!');
 } else {
-    getChampionName(sport);
+    getChampionName(sport.toLowerCase());
 }
 
 function getCountry(city) {
     switch (city) {
-        case 'Київ':
-        case 'Kyiv':
+        case 'київ':
+        case 'kyiv':
             message += 'You live in the capital of Ukraine. ';
             break;
-        case 'Вашингтон':
-        case 'Washington':
+        case 'вашингтон':
+        case 'washington':
             message += 'You live in the capital of the United States of America. ';
             break;
-        case 'Лондон':
-        case 'London':
+        case 'лондон':
+        case 'london':
             message += 'You live in the capital of Great Britain. ';
             break;
         default:
-            message += `You live in the city of ${city}. `;
+            message += `You live in the city of ${upFirsLetter(city)}. `;
     }
 
     return message;
