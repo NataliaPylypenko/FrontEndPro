e48ad5e6347e79c7407647cf94d5e4e452931570
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-10-17 17:12:03 +0300

    homework13

--- 13/js/main.js
+++ 13/js/main.js
@@ -1,8 +1,12 @@
-function removeElement(arr, num) {
-    delete arr[arr.indexOf(num)];
-    return arr;
+function removeElement(arr, element) {
+    const indexOfElement = arr.indexOf(element);
+    if (indexOfElement === -1) return arr;
+
+    return arr.splice(indexOfElement, 1);
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7];
-removeElement(array, 5 );
+const itemForRemove = 5;
+
+removeElement(array, itemForRemove);
 console.log(array);
