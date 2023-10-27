2a6cfdef12e3c27c2ec785cb86e5f0f6aab545b8
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-10-27 11:33:05 +0300

    homework17

--- 17/js/main.js
+++ 17/js/main.js
@@ -1,17 +1,21 @@
 // Написати функцію, яка приймає 1 параметр. з тим, що передали перший раз і т. д.
 
 /**
+ * 1
  * Global env
  * Record: {}
  * Parent: null
  */
 
 /**
+ * 3
  * [[Environment]] = Global env
  */
 
 function func() {
+
     /**
+     * 5
      * func env
      * Record: {}
      * Parent: Global env
@@ -20,62 +24,109 @@ function func() {
     let sum = 0;
 
     /**
+     * 6
      * func env
      * Record: {sum: 0}
      * Parent: Global env
      */
 
-    return num => console.log(sum += num)
+    /**
+     * 10
+     * func env
+     * Record: {sum: 3}
+     * Parent: Global env
+     */
+
+    /**
+     * 13
+     * func env
+     * Record: {sum: 8}
+     * Parent: Global env
+     */
+
+    /**
+     * 16
+     * func env
+     * Record: {sum: 16}
+     * Parent: Global env
+     */
+
+    /**
+     * 7
+     * [[Environment]] = func env
+     */
+
+    return function(num) {
+
+        /**
+         * 9
+         * anonymous env
+         * Record: {num: 3}
+         * Parent: func env
+         */
+
+        /**
+         * 12
+         * anonymous env
+         * Record: {num: 5}
+         * Parent: func env
+         */
+
+        /**
+         * 15
+         * anonymous env
+         * Record: {num: 20}
+         * Parent: func env
+         */
+
+        sum += num;
+
+        return sum;
+    }
+
 }
 
 /**
+ * 4
  * Global env
  * Record: {func: <function>}
  * Parent: null
  */
 
 const sum = func();
-/**
- * sum env
- * Record: {}
- * Parent: func env
- */
-
-sum(3);
-/**
- * sum env
- * Record: {num: 3}
- * Parent: func env
- */
 
 /**
- * func env
- * Record: {sum: 3}
- * Parent: Global env
+ * 8
+ * Global env
+ * Record: {func: <function>, sum: <function>}
+ * Parent: null
  */
 
-sum(5);
-/**
- * sum env
- * Record: {num: 5}
- * Parent: func env
- */
+const result1 = sum(3);
 
 /**
- * func env
- * Record: {sum: 8}
- * Parent: Global env
+ * 11
+ * Global env
+ * Record: {func: <function>, sum: <function>, result1: 3}
+ * Parent: null
  */
 
-sum(20);
-/**
- * sum env
- * Record: {num: 20}
- * Parent: func env
- */
+const result2 = sum(5);
 
 /**
- * func env
- * Record: {sum: 28}
- * Parent: Global env
+ * 14
+ * Global env
+ * Record: {func: <function>, sum: <function>, result1: 3, result2: 8}
+ * Parent: null
  */
+
+const result3 = sum(20);
+
+/**
+ * 17
+ * Global env
+ * Record: {func: <function>, sum: <function>, result1: 3, result2: 8, result3: 28}
+ * Parent: null
+ */
+
+const sum2 = func();
