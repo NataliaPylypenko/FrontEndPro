c97e60f7f7f9eaaf6743028a9a1762cefa9b8ad2
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-11-07 14:18:04 +0200

    homework19

--- 19/js/main.js
+++ 19/js/main.js
@@ -46,7 +46,9 @@ class Flat {
     residents = [];
 
     addResident(person) {
-        return this.residents.push(person);
+        person instanceof Person
+            ? this.residents.push(person)
+            : console.log('Error: Add an instance of the Person class!');
     }
 }
 
@@ -58,9 +60,11 @@ class House {
     };
 
     addFlat(flat) {
-        return this.flats.length < this.maxApartments
-            ? this.flats.push(flat)
-            : console.log(`This house contains only ${this.maxApartments} apartments`);
+        this.flats.length < this.maxApartments
+            ? flat instanceof Flat
+                ? this.flats.push(flat)
+                : console.log('Error: Add an instance of the Flat class!')
+            : console.log(`Error: This house contains only ${this.maxApartments} apartments`);
     }
 }
 
@@ -74,12 +78,13 @@ let person7 = new Person('Dipper', 'male');
 let person8 = new Person('Mabel', 'female');
 let person9 = new Person('Wendy', 'female');
 let person10 = new Person('Bender', 'another');
+let person11 = 'Anna';
 
 let flat1 = new Flat();
 let flat2 = new Flat();
 let flat3 = new Flat();
 let flat4 = new Flat();
-let flat5 = new Flat();
+let flat5 = 'apartments';
 let flat6 = new Flat();
 
 flat1.addResident(person1);
@@ -87,19 +92,19 @@ flat1.addResident(person2);
 flat1.addResident(person3);
 flat1.addResident(person4);
 flat1.addResident(person5);
-
 flat2.addResident(person6);
 flat2.addResident(person7);
 flat2.addResident(person8);
-
 flat3.addResident(person9);
-
 flat4.addResident(person10);
+flat6.addResident(person11);
 
 console.log(flat1.residents);
 console.log(flat2.residents);
 console.log(flat3.residents);
 console.log(flat4.residents);
+console.log(flat5.residents);
+console.log(flat6.residents);
 
 let house = new House(5);
 
@@ -109,3 +114,5 @@ house.addFlat(flat3);
 house.addFlat(flat4);
 house.addFlat(flat5);
 house.addFlat(flat6);
+
+console.log(house.flats);
