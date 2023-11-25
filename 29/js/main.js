1c5837023da9202bd482ab9bbcc1c2d74f0c893a
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-11-25 16:43:38 +0200

    task29

--- 29/js/main.js
+++ 29/js/main.js
@@ -10,95 +10,118 @@
     анімації можна використовувати transition.
  */
 
-const body = document.querySelector('body');
-const block = document.querySelector('.block');
-const boom = document.querySelector('.boom');
-const bodyWidth = body.clientWidth;
-const bodyHeight = body.clientHeight;
-const blockWidth = block.clientWidth;
-const blockHeight = block.clientHeight;
-const STEP_SIZE = 10;
+function Box(block) {
+    const body = document.querySelector('body');
+    const boom = document.querySelector('.boom');
+    const bodyWidth = body.clientWidth;
+    const bodyHeight = body.clientHeight;
+    const blockWidth = block.clientWidth;
+    const blockHeight = block.clientHeight;
+    const STEP_SIZE = 10;
 
-const showBoom = () => {
-    boom.style.display = 'block';
-    setTimeout(() => boom.style.display = 'none', 2000);
-};
+    const showBoom = () => {
+        boom.style.display = 'block';
+        setTimeout(() => boom.style.display = 'none', 2000);
+    };
 
-const updatePosition = (direction, prop) => {
-    if (block.style[prop] === '') block.style[prop] = '0';
+    block.style.top = `${(bodyHeight - blockHeight) / 2}px`;
+    block.style.left = `${(bodyWidth - blockWidth) / 2}px`;
 
-    const blockStyleProp = parseInt(block.style[prop]);
-    const movement = (prop === 'left') ? (bodyWidth - blockWidth) / 2 : (bodyHeight - blockHeight) / 2;
+    return {
+        moveUp: () => {
+            if (parseInt(block.style.top) <= 0) {
+                showBoom();
+                block.style.top = `${STEP_SIZE * 2}px`;
+                return;
+            }
+            block.style.top = `${parseInt(block.style.top) - STEP_SIZE}px`
+        },
+        moveDown: () => {
+            if (parseInt(block.style.top) + blockHeight >= bodyHeight) {
+                showBoom();
+                block.style.top = `${bodyHeight - blockHeight - STEP_SIZE * 2}px`;
+                return;
+            }
+            block.style.top = `${parseInt(block.style.top) + STEP_SIZE}px`;
+        },
+        moveRight: () => {
+            if (parseInt(block.style.left) + blockWidth >= bodyWidth) {
+                showBoom();
+                block.style.left = `${bodyWidth - blockWidth - STEP_SIZE * 2}px`;
+                return;
+            }
+            block.style.left = `${parseInt(block.style.left) + STEP_SIZE}px`;
+        },
+        moveLeft: () => {
+            if (parseInt(block.style.left) <= 0) {
+                showBoom();
+                block.style.left = `${STEP_SIZE * 2}px`;
+                return;
+            }
+            block.style.left = `${parseInt(block.style.left) - STEP_SIZE}px`;
+        },
+        bounce: () => {
+            block.style.top = `${parseInt(block.style.top) - STEP_SIZE}px`;
 
-    if (direction === 'right' || direction === 'bottom') {
-        if (movement - STEP_SIZE >= blockStyleProp) {
-            block.style[prop] = `${blockStyleProp + STEP_SIZE}px`;
-        } else {
-            block.style[prop] = `${movement - 2 * STEP_SIZE}px`;
-            showBoom();
+            if (parseInt(block.style.top) <= 0) {
+                showBoom();
+                block.style.top = `${STEP_SIZE * 2}px`;
+                return;
+            }
+
+            setTimeout(() => block.style.top = `${parseInt(block.style.top) + STEP_SIZE}px`, 400)
+        },
+        satDown: () => {
+            block.style.height = `${blockHeight - blockHeight * 0.4}px`;
+            block.style.top = `${parseInt(block.style.top) + blockHeight * 0.2}px`;
+
+            block.style.width = `${blockWidth + blockWidth * 0.25}px`;
+            block.style.left = `${parseInt(block.style.left) - blockWidth * 0.25 / 2}px`;
+
+            setTimeout(() => {
+                block.style.height = `${blockHeight}px`;
+                block.style.top = `${parseInt( block.style.top) - blockHeight * 0.2}px`;
+                block.style.width = `${blockWidth}px`;
+                block.style.left = `${parseInt(block.style.left) + blockWidth * 0.25 / 2}px`;
+            }, 400);
+
+            if (parseInt(block.style.left) + parseInt(block.style.width) >= bodyWidth) {
+                showBoom();
+                block.style.left = `${bodyWidth - blockWidth - STEP_SIZE * 3}px`;
+                return;
+            }
+
+            if (parseInt(block.style.left) <= 0) {
+                showBoom();
+                block.style.left = `${STEP_SIZE * 2}px`;
+                return;
+            }
         }
-    } else {
-        if (movement - STEP_SIZE >= -blockStyleProp) {
-            block.style[prop] = `${blockStyleProp - STEP_SIZE}px`;
-        } else {
-            block.style[prop] = `${-movement + 2 * STEP_SIZE}px`;
-            showBoom();
-        }
-    }
-};
+    };
+}
 
-const handleKeydownArrow = (direction) => {
-    if (direction === 'right' || direction === 'left') {
-        updatePosition(direction, 'left');
-    } else {
-        updatePosition(direction, 'top');
-    }
-};
-
-const handleKeydownSpace = () => {
-    if (block.style.top === '') block.style.top = '0';
-
-    block.style.top = `${parseInt(block.style.top) - 10}px`;
-
-    setTimeout(() => block.style.top = `${parseInt(block.style.top) + 10}px`, 500);
-};
-
-const handleKeydownCtrl = () => {
-    if (block.style.width === '') block.style.width = '100px';
-    if (block.style.height === '') block.style.height = '100px';
-
-    const blockClientWidth = block.clientWidth * 0.25;
-    const blockClientHeight = block.clientHeight * 0.4;
-
-    block.style.width = `${block.clientWidth + blockClientWidth}px`;
-    block.style.height = `${block.clientHeight - blockClientHeight}px`;
-
-    setTimeout(() => {
-        block.style.width = `${block.clientWidth - blockClientWidth}px`;
-        block.style.height = `${block.clientHeight + blockClientHeight}px`;
-    }, 500);
-};
+const box = new Box(document.querySelector('.block'));
 
 const handleKeydown = (e) => {
     switch (e.code) {
         case 'ArrowRight':
-            handleKeydownArrow('right');
+            box.moveRight();
             break;
         case 'ArrowLeft':
-            handleKeydownArrow('left');
+            box.moveLeft();
             break;
         case 'ArrowUp':
-            handleKeydownArrow('top');
+            box.moveUp();
             break;
         case 'ArrowDown':
-            handleKeydownArrow('bottom');
+            box.moveDown();
             break;
         case 'Space':
-            handleKeydownSpace();
+            box.bounce();
             break;
         case 'ControlLeft':
         case 'ControlRight':
-            handleKeydownCtrl();
+            box.satDown();
             break;
     }
 };
