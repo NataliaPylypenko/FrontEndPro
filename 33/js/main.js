1c8f90cf0ad51305d6e527ee030e0c15510e9b84
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-11-30 10:19:54 +0200

    render product list

--- 33/js/main.js
+++ 33/js/main.js
@@ -62,69 +62,139 @@ const categories = [
 class CategoryList {
     constructor(element) {
         this.element = element;
-        this.render();
     }
 
-    unsetActiveCategory(item) {
-        item.classList.remove('active');
-    }
-
-    setActiveCategory(item) {
-        item.classList.add('active');
-    }
-
-    generateCategories() {
+    generateCategories(categories) {
         const ul = document.createElement('ul');
-        ul.classList.add('categoriesList');
+        ul.classList.add('list');
 
         categories.forEach((category, i) => {
             const li = document.createElement('li');
             const a = document.createElement('a');
             a.setAttribute("href", "#");
+            a.setAttribute("data-category-id", `${category.id}`);
             a.innerHTML = category.name;
             li.appendChild(a);
             ul.appendChild(li);
 
-            i === 0 && this.setActiveCategory(a);
+            // i === 0 && this.setActiveItem(a);
         });
 
         return ul;
     }
 
-    render() {
-        this.element.appendChild(this.generateCategories());
+    // 1
+    render(categories) {
+        this.element.appendChild(this.generateCategories(categories));
     }
 
+    // 2
     onCategorySelect(callback) {
         this.element.addEventListener('click', callback);
     }
 }
 
-class AppController {
-    constructor(categoryList) {
-        this.categoryList = categoryList;
+class ProductList {
+    constructor(element) {
+        this.element = element;
     }
 
+    generateProducts(products) {
+        const ul = document.createElement('ul');
+        ul.classList.add('list');
+
+        products.forEach((product, i) => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.setAttribute("href", "#");
+            a.setAttribute("data-product-id", `${product.id}`);
+            a.innerHTML = product.name;
+            li.appendChild(a);
+            ul.appendChild(li);
+        });
+
+        return ul;
+    }
+
+    // 6
+    render(products) {
+        this.element.appendChild(this.generateProducts(products))
+    }
+
+    // 7
+    onProductSelect(callback) {
+        this.element.addEventListener('click', callback);
+    }
+}
+
+class AppController {
+    constructor(categories, categoryList, productList) {
+        this.categoryList = categoryList;
+        this.productList = productList;
+        this.categories = categories;
+        this.products = [];
+    }
+
+    // 1
     initialize() {
+        this.categoryList.render(this.categories);
+
+        // 2
         this.categoryList.onCategorySelect((e) => {
+            // 3
             this.handlerClick(e);
-            const products = this.getProductsByCategory(1);
-            console.log(products);
+            // 4
+            const categoryId = parseInt(e.target.getAttribute('data-category-id'));
+            // 5
+            this.products = this.getProductsByCategory(categoryId);
+            // 6
+            this.reset();
+            this.productList.render(this.products);
+        });
+
+        // 7
+        this.productList.onProductSelect((e) => {
+            // 8
+            this.handlerClick(e);
+            // 9
+            const productId = parseInt(e.target.getAttribute('data-product-id'));
+            // 10
+            const product = this.getProductDetails(productId);
         });
     }
 
+    unsetActiveItem(item) {
+        item.classList.remove('active');
+    }
+
+    setActiveItem(item) {
+        item.classList.add('active');
+    }
+
+    // 3 // 8
     handlerClick = (e) => {
-        const currentActiveLi = e.currentTarget.querySelector('.active');
-        currentActiveLi && this.categoryList.unsetActiveCategory(currentActiveLi);
-        this.categoryList.setActiveCategory(e.target);
+        const currentActiveItem = e.currentTarget.querySelector('.active');
+        currentActiveItem && this.unsetActiveItem(currentActiveItem);
+        this.setActiveItem(e.target);
     };
 
+    // 5
     getProductsByCategory(categoryId) {
-        const category = categories.find(category => category.id === categoryId);
+        const category = this.categories.find(category => category.id === categoryId);
         return category ? category.products : [];
+    }
+    // 10
+    getProductDetails(productId) {
+        const product = this.products.find(product => product.id === productId);
+        return product ? product : {};
+    }
+
+    reset() {
+        this.productList.element.innerHTML = '';
     }
 }
 
 const categoryList = new CategoryList(document.querySelector('#categoriesBlock'));
-const appController = new AppController(categoryList);
+const productList = new ProductList(document.querySelector('#productsBlock'));
+const appController = new AppController(categories, categoryList, productList);
 appController.initialize();
