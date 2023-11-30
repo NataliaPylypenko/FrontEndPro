79421a3c5324fc9a9966f857fe8f3938fcf7c95d
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-11-30 12:04:30 +0200

    render product details

--- 33/js/main.js
+++ 33/js/main.js
@@ -16,12 +16,14 @@ const categories = [
             {
                 "id": 101,
                 "name": "Смартфон",
-                "price": 599.99
+                "price": 599.99,
+                "src": 'https://c.dns-shop.ru/thumb/st1/fit/760/600/6f6fcc851d6c7bd2ea61a2a5196c7888/q93_0ca639872faed3929b880c383288085297021c45b5f9812d8ec008220f2c0672.png'
             },
             {
                 "id": 102,
                 "name": "Ноутбук",
-                "price": 1299.99
+                "price": 1299.99,
+                "src": ''
             }
         ]
     },
@@ -32,12 +34,14 @@ const categories = [
             {
                 "id": 201,
                 "name": "Футболка",
-                "price": 19.99
+                "price": 19.99,
+                "src": ''
             },
             {
                 "id": 202,
                 "name": "Джинси",
-                "price": 49.99
+                "price": 49.99,
+                "src": ''
             }
         ]
     },
@@ -48,12 +52,14 @@ const categories = [
             {
                 "id": 301,
                 "name": "Роман",
-                "price": 14.99
+                "price": 14.99,
+                "src": ''
             },
             {
                 "id": 302,
                 "name": "Наукова книга",
-                "price": 24.99
+                "price": 24.99,
+                "src": ''
             }
         ]
     }
@@ -127,12 +133,46 @@ class ProductList {
     }
 }
 
+class ProductDetails {
+    constructor(element) {
+        this.element = element;
+    }
+
+    generateProduct(product) {
+        const div = document.createElement('div');
+        div.classList.add('card');
+
+        const child = `
+            <div class="card__img">
+                <img src="${product.src}" style="width:100%">
+            </div>
+            <h1>${product.name}</h1>
+            <p class="price">${product.price}</p>
+            <p><button class="btn success" id="toBuy">To Buy</button></p>
+        `;
+
+        div.innerHTML = child;
+
+        return div;
+    }
+
+    render(product) {
+        this.element.appendChild(this.generateProduct(product))
+    }
+
+    onBuy(callback) {
+        this.element.addEventListener('click', callback);
+    }
+}
+
 class AppController {
-    constructor(categories, categoryList, productList) {
+    constructor(categories, categoryList, productList, productDetails) {
         this.categoryList = categoryList;
         this.productList = productList;
+        this.productDetails = productDetails;
         this.categories = categories;
         this.products = [];
+        this.product = [];
     }
 
     // 1
@@ -148,7 +188,7 @@ class AppController {
             // 5
             this.products = this.getProductsByCategory(categoryId);
             // 6
-            this.reset();
+            this.resetProductList();
             this.productList.render(this.products);
         });
 
@@ -159,8 +199,21 @@ class AppController {
             // 9
             const productId = parseInt(e.target.getAttribute('data-product-id'));
             // 10
-            const product = this.getProductDetails(productId);
+            this.product = this.getProductDetails(productId);
+            // 11
+            this.resetProductDetails();
+            this.productDetails.render(this.product);
         });
+
+        // 12
+        this.productDetails.onBuy((e) => {
+            // 13
+            this.purchaseProduct(this.product);
+
+            //
+            this.resetProductList();
+            this.resetProductDetails();
+        })
     }
 
     unsetActiveItem(item) {
@@ -183,18 +236,29 @@ class AppController {
         const category = this.categories.find(category => category.id === categoryId);
         return category ? category.products : [];
     }
+
     // 10
     getProductDetails(productId) {
         const product = this.products.find(product => product.id === productId);
         return product ? product : {};
     }
 
-    reset() {
+    // 13
+    purchaseProduct(product) {
+        alert(`Товар ${product.name} куплений!`);
+    }
+
+    resetProductList() {
         this.productList.element.innerHTML = '';
+    }
+    resetProductDetails() {
+        this.productDetails.element.innerHTML = '';
     }
 }
 
 const categoryList = new CategoryList(document.querySelector('#categoriesBlock'));
 const productList = new ProductList(document.querySelector('#productsBlock'));
-const appController = new AppController(categories, categoryList, productList);
+const productDetails = new ProductDetails(document.querySelector('#productDetailsBlock'));
+
+const appController = new AppController(categories, categoryList, productList, productDetails);
 appController.initialize();
