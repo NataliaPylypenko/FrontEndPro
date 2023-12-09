d3416a0dd09be064a32b33f131684079b13b79af
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-12-09 17:59:31 +0200

    task35

--- 35/js/task.js
+++ 35/js/task.js
@@ -3,7 +3,67 @@
   ATM
  */
 
-const getUserResponseNotEmpty = (message, data) => {
+const userData = {
+    USD: 1000,
+    EUR: 900,
+    UAH: 15000,
+    BIF: 20000,
+    AOA: 100
+};
+
+const bankData = {
+    USD: {
+        max: 3000,
+        min: 100,
+        img: '💵'
+    },
+    EUR: {
+        max: 1000,
+        min: 50,
+        img: '💶'
+    },
+    UAH: {
+        max: 0,
+        min: 0,
+        img: '💴'
+    },
+    GBP: {
+        max: 10000,
+        min: 100,
+        img: '💷'
+    }
+};
+
+const geSuitableCurrency = (userData, bankData, userResponse) => {
+    if (!bankData.hasOwnProperty(userResponse) || !userData.hasOwnProperty(userResponse)) {
+        return false;
+    } else if (bankData[userResponse].max === 0) {
+        alert('At the moment there are no notes of this currency!');
+        return false;
+    } else if (userData[userResponse] < bankData[userResponse].min) {
+        alert('You don\'t have enough funds!');
+        return false;
+    }
+
+    return true;
+};
+
+const geSuitableInteger = (userData, bankData, currency, userResponse) => {
+    const min = bankData[currency]['min'];
+    const max = Math.min(bankData[currency]['max'], userData[currency]);
+
+    if (userResponse > max) {
+        alert(`The amount entered is greater than available. Maximum withdrawal amount: ${max}`);
+        return false;
+    } else if (userResponse <= min) {
+        alert(`The amount entered is less than available. Minimum withdrawal amount: ${min}`);
+        return false;
+    }
+
+    return true;
+};
+
+const getUserResponse = (data, message) => {
     let userResponse;
 
     do {
@@ -13,45 +73,49 @@ const getUserResponseNotEmpty = (message, data) => {
     return userResponse;
 };
 
-const getMoney = file => {
+const getUserResponseCurrency = (userData, bankData, message) => {
+    let userResponse;
+
+    do {
+        userResponse = prompt(message);
+    } while (!geSuitableCurrency(userData, bankData, userResponse));
+
+    return userResponse;
+};
+
+const getUserResponseInteger = (userData, bankData, currency, message) => {
+    let userResponse;
+
+    do {
+        userResponse = parseInt(prompt(message));
+    } while (!geSuitableInteger(userData, bankData, currency, userResponse));
+
+    return userResponse;
+};
+
+const getMoney = (userData, bankData) => {
     return new Promise(
         (resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', file);
-            xhr.send();
-
-            xhr.addEventListener('readystatechange', () => {
-                if(xhr.readyState === 4) {
-                    const isStatus = confirm('View the balance card?'); // ???
-                    const result = JSON.parse(xhr.response);
-                    const status = xhr.status;
-
-                    isStatus ? resolve(result) : reject(status);
-                }
-            })
+            const isStatus = confirm('View the balance card?');
+            isStatus ? resolve(userData) : reject({userData, bankData});
         }
     )
 };
 
-getMoney('./files/userData.json')
+getMoney(userData, bankData)
     .then(
-        data => {
-            const userResponse = getUserResponseNotEmpty('Enter the currency in which the balance will be displayed!', data);
-            alert(`${data[userResponse]} ${userResponse}`)
+        (userData) => {
+            const currency = getUserResponse(userData, 'Enter the currency in which the balance will be displayed!');
+            alert(`The balance is: ${userData[currency]} ${currency}`)
+        },
+        ({userData, bankData}) => {
+            const currency = getUserResponseCurrency(userData, bankData, 'Enter the currency for cash withdrawal');
+            const amount = getUserResponseInteger(userData, bankData, currency, 'Enter the withdrawal amount');
+
+            alert(`Here is your money ${amount} ${currency} ${bankData[currency]['img']}`);
         }
     )
-    .catch(
-        () => {
-            return getFile('./files/bankData.json')
-        }
-    )
-    .catch(
-        data => {
-            prompt('Enter the currency in which the cash withdrawal will be made!')
-        }
-    )
-    .finally(
-        () => {
-            alert('Thank you, have a nice day 😊');
-        }
-    );
+    .catch(() => console.error(new Error('Something went wrong')))
+    .finally(() => alert('Thank you, have a nice day 😊'));
+
+
