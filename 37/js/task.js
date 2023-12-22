c32dd90949b848a1c5a235c838936cf4aa452981
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-12-22 15:08:45 +0200

    async...await

--- 37/js/task.js
+++ 37/js/task.js
@@ -1,12 +1,22 @@
+/* =========== async...await =========== */
+
 const refs = {
     apiUrl: 'https://jsonplaceholder.typicode.com',
     postForm: document.querySelector('#postForm'),
     posts: document.querySelector('#posts'),
 };
 
-const fetchData = url => fetch(url)
-    .then(data => data.ok ? data.json() : Promise.reject(data.statusText))
-    .catch(err => console.error(err));
+const getData = async (url) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (err) {
+        console.error(err)
+    }
+};
 
 const renderPost = (post) => {
     const postItem = document.createElement('div');
@@ -20,9 +30,7 @@ const renderPost = (post) => {
         <button type="button" class="card__button" data-id="${post.id}">Comments</button>
     `;
 
-    // refs.posts.appendChild(postItem);
     refs.postForm.insertAdjacentElement('afterEnd', postItem);
-
     document.querySelector('.card__button').addEventListener('click', handlerClickBtn);
 };
 
@@ -39,36 +47,113 @@ const renderComments = (comment) => {
         </div>
     `;
 
-    // refs.posts.appendChild(commentItem);
     const post = document.querySelector(`[data-post="${comment.postId}"]`);
     post.insertAdjacentElement('afterEnd', commentItem);
 };
 
 
-const getPost = (params) => fetchData(`${refs.apiUrl}${params}`)
+const getPost = (params) => getData(`${refs.apiUrl}${params}`)
     .then(data => renderPost(data))
     .catch(err => console.error(err));
 
-const getComments = (params) => fetchData(`${refs.apiUrl}${params}`)
-    .then(comments => comments.forEach(comment => renderComments(comment)))
+const getComments = (params) => getData(`${refs.apiUrl}${params}`)
+    .then(data => data.forEach(obj => renderComments(obj)))
     .catch(err => console.error(err));
 
 
-const handlerSubmitForm = (e) => {
+const handlerSubmitForm = async (e) => {
     e.preventDefault();
 
     const idPost = +refs.postForm.querySelector('input[name="idPost"]').value;
-    getPost(`/posts/${idPost}`);
+    await getPost(`/posts/${idPost}`);
 
     refs.postForm.reset();
 };
 
-const handlerClickBtn = (e) => {
+const handlerClickBtn = async (e) => {
     e.preventDefault();
 
     const idComments = e.target.dataset.id;
-    getComments(`/comments?postId=${idComments}`);
+    await getComments(`/comments?postId=${idComments}`);
 };
 
 
 refs.postForm.addEventListener('submit', handlerSubmitForm);
+
+
+/* =========== Promises =========== */
+
+// const refs = {
+//     apiUrl: 'https://jsonplaceholder.typicode.com',
+//     postForm: document.querySelector('#postForm'),
+//     posts: document.querySelector('#posts'),
+// };
+//
+// const fetchData = url => fetch(url)
+//     .then(data => data.ok ? data.json() : Promise.reject(data.statusText))
+//     .catch(err => console.error(err));
+//
+// const renderPost = (post) => {
+//     const postItem = document.createElement('div');
+//     postItem.className = 'card w3-card-4';
+//     postItem.setAttribute('data-post', `${post.id}`);
+//     postItem.innerHTML = `
+//         <h3 class="card__header">${post.title}</h3>
+//         <div class="card__body">
+//           <p>${post.body}</p>
+//         </div>
+//         <button type="button" class="card__button" data-id="${post.id}">Comments</button>
+//     `;
+//
+//     // refs.posts.appendChild(postItem);
+//     refs.postForm.insertAdjacentElement('afterEnd', postItem);
+//
+//     document.querySelector('.card__button').addEventListener('click', handlerClickBtn);
+// };
+//
+// const renderComments = (comment) => {
+//     const commentItem = document.createElement('div');
+//     commentItem.className = 'card w3-card-4 comment';
+//     commentItem.innerHTML = `
+//         <div class="card__header">
+//           <h4>${comment.name}</h4>
+//           <h6>${comment.email}</h6>
+//         </div>
+//         <div class="card__body">
+//           <p>${comment.body}</p>
+//         </div>
+//     `;
+//
+//     // refs.posts.appendChild(commentItem);
+//     const post = document.querySelector(`[data-post="${comment.postId}"]`);
+//     post.insertAdjacentElement('afterEnd', commentItem);
+// };
+//
+//
+// const getPost = (params) => fetchData(`${refs.apiUrl}${params}`)
+//     .then(data => renderPost(data))
+//     .catch(err => console.error(err));
+//
+// const getComments = (params) => fetchData(`${refs.apiUrl}${params}`)
+//     .then(comments => comments.forEach(comment => renderComments(comment)))
+//     .catch(err => console.error(err));
+//
+//
+// const handlerSubmitForm = (e) => {
+//     e.preventDefault();
+//
+//     const idPost = +refs.postForm.querySelector('input[name="idPost"]').value;
+//     getPost(`/posts/${idPost}`);
+//
+//     refs.postForm.reset();
+// };
+//
+// const handlerClickBtn = (e) => {
+//     e.preventDefault();
+//
+//     const idComments = e.target.dataset.id;
+//     getComments(`/comments?postId=${idComments}`);
+// };
+//
+//
+// refs.postForm.addEventListener('submit', handlerSubmitForm);
