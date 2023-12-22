50b1fdb369700698d73b3e62bff725b50bb5514f
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-12-22 10:08:50 +0200

    display comments

--- 37/js/task.js
+++ 37/js/task.js
@@ -11,6 +11,7 @@ const fetchData = url => fetch(url)
 const renderPost = (post) => {
     const postItem = document.createElement('div');
     postItem.className = 'card w3-card-4';
+    postItem.setAttribute('data-post', `${post.id}`);
     postItem.innerHTML = `
         <h3 class="card__header">${post.title}</h3>
         <div class="card__body">
@@ -19,23 +20,28 @@ const renderPost = (post) => {
         <button type="button" class="card__button" data-id="${post.id}">Comments</button>
     `;
 
-    refs.posts.appendChild(postItem);
+    // refs.posts.appendChild(postItem);
+    refs.postForm.insertAdjacentElement('afterEnd', postItem);
 
     document.querySelector('.card__button').addEventListener('click', handlerClickBtn);
 };
 
 const renderComments = (comment) => {
-    const postItem = document.createElement('div');
-    postItem.className = 'card w3-card-4 comment';
-    postItem.innerHTML = `
-        <h3 class="card__header">${comment.name}</h3>
-        <h5 class="card__header">${comment.email}</h5>
+    const commentItem = document.createElement('div');
+    commentItem.className = 'card w3-card-4 comment';
+    commentItem.innerHTML = `
+        <div class="card__header">
+          <h4>${comment.name}</h4>
+          <h6>${comment.email}</h6>
+        </div>
         <div class="card__body">
           <p>${comment.body}</p>
         </div>
     `;
 
-    refs.posts.appendChild(postItem)
+    // refs.posts.appendChild(commentItem);
+    const post = document.querySelector(`[data-post="${comment.postId}"]`);
+    post.insertAdjacentElement('afterEnd', commentItem);
 };
 
 
@@ -61,7 +67,7 @@ const handlerClickBtn = (e) => {
     e.preventDefault();
 
     const idComments = e.target.dataset.id;
-    getComments(`/comments?postId=${idComments}`)    ;
+    getComments(`/comments?postId=${idComments}`);
 };
 
 
